d60a7c9dbf879d8f5171a7ce6b78e64486d6d2bb
Jazz Parayaoan <jparayaoan19@mail.wou.edu>
2023-02-28 17:56:49 -0800

    adding stuff that visual studio was mad with me for

--- src/DWC_NightOwlProject/DWC_NightOwlProject/Data/WebAppDbContext.cs
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/Data/WebAppDbContext.cs
@@ -28,17 +28,25 @@ public partial class WebAppDbContext : DbContext
     {
         modelBuilder.Entity<Material>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Material__3214EC27DA8504B8");
+            entity.HasKey(e => e.Id).HasName("PK__Material__3214EC2785581C70");
+
+            entity.HasOne(d => d.Template).WithMany(p => p.Materials)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Material_Fk_Template");
+
+            entity.HasOne(d => d.World).WithMany(p => p.Materials)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Material_Fk_World");
         });
 
         modelBuilder.Entity<Template>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Template__3214EC27D88AD481");
+            entity.HasKey(e => e.Id).HasName("PK__Template__3214EC27F3675E38");
         });
 
         modelBuilder.Entity<World>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__World__3214EC27A3C2934D");
+            entity.HasKey(e => e.Id).HasName("PK__World__3214EC27495DB421");
         });
 
         OnModelCreatingPartial(modelBuilder);
